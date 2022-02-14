e2998d6c715b7e38aecbb6e86308806b0ce22ac7
leubenia <hdodo777@gmail.com>
2022-02-14 23:13:45 +0900

    추가 sql 연결 및 packge json start script

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const { sequelize, Sequelize } = require('./models');
 
+const driver = async () => {
+    try {
+      await sequelize.sync();
+    } catch (err) {
+      console.error('초기화 실패');
+      console.error(err);
+      return;
+    }
+    console.log('초기화 완료.');
+  }
+driver();
 const app = express();
 
 app.use('/',(req, res)=>{
