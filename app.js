44cd3d511bb81ae18f48f4f6b393a8d42a9bfdfd
leubenia <hdodo777@gmail.com>
2022-03-03 17:46:20 +0900

    프리티어

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-const express = require("express");
+const express = require('express');
 const { sequelize, Sequelize, mytest } = require('./models');
-const router = require("./router")
+const router = require('./router');
 
 const driver = async () => {
-    try {
-      await sequelize.sync();
-    } catch (err) {
-      console.error('초기화 실패');
-      console.error(err);
-      return;
-    }
-    console.log('초기화 완료.');
+  try {
+    await sequelize.sync();
+  } catch (err) {
+    console.error('초기화 실패');
+    console.error(err);
+    return;
   }
+  console.log('초기화 완료.');
+};
 driver();
 const app = express();
 app.use(express.json());
 app.use('/', router);
 
-
-app.listen(3000,()=> {
-    console.log("http://localhost:3000");
-})
+app.listen(3000, () => {
+  console.log('http://localhost:3000');
+});
